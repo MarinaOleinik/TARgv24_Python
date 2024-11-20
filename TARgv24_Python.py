11caa478afc631676b107a421c89cfcb22274317
marinaoleinik <marina.oleinik@tthk.ee>
2024-11-20 16:01:38 +0200

    Ülesanne 2

--- TARgv24_Python.py
+++ TARgv24_Python.py
@@ -1,8 +1,16 @@
 #Ülesanne 1
-print("Tere tulemast!")
-nimi=input("Mis on sinu nimi? ")
-print("Tere tulemast! Tervitan sind ", nimi) 
-print("Tere tulemast! Tervitan sind "+ nimi)
-vanus=int(input("Kui vana sa oled? "))
-print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
-print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
+# print("Tere tulemast!")
+# nimi=input("Mis on sinu nimi? ")
+# print("Tere tulemast! Tervitan sind ", nimi) 
+# print("Tere tulemast! Tervitan sind "+ nimi)
+# vanus=int(input("Kui vana sa oled? "))
+# print("Tere tulemast! Tervitan sind "+nimi+" Sa oled ",vanus,"aastat vana")
+# print(f"\tTere tulemast! \nTervitan sind {nimi} Sa oled {vanus} aastat vana")
+
+#Ülesanne 2
+vanus = 18
+eesnimi = "Jaak" 
+pikkus = 16.5
+kas_käib_koolis = True
+print(type(vanus))
+
